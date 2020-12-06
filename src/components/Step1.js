fb0cdc0846b640e0d17aa48143c43b30b94aaceb
Andrii Stepanets <ovrass@gmail.com>
2020-12-06 20:53:12 +0200

    Added email input in step two

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Layout from './Layout';
 import styled from 'styled-components';
 import { MdPublic } from 'react-icons/md';
@@ -19,13 +20,21 @@ const StepStyles = styled.h2`
 // using yup
 
 const schema = yup.object().shape({
-    firstName: yup.string().matches(/^([^0-9]*)$/, "First name should not contain numbers").required("First name is a required field"),
-    lastName: yup.string().matches(/^([^0-9]*)$/, "Last name should not contain numbers").required("Last name is a required field"),
+                    firstName: yup
+                                .string()
+                                .matches(/^([^0-9]*)$/, "First name should not contain numbers")
+                                .required("First name is a required field"),
+                    lastName: yup
+                                .string()
+                                .matches(/^([^0-9]*)$/, "Last name should not contain numbers")
+                                .required("Last name is a required field"),
 
 })
 
 
 export default function Step1() {
+
+    const history = useHistory();
 
     const { register, handleSubmit, errors } = useForm({
         mode: "onBlur",
@@ -33,7 +42,7 @@ export default function Step1() {
     })
 
     const onSubmit = (data) => {
-        console.log(data);
+        history.push('/step2')
     }
 
     return (
@@ -42,8 +51,22 @@ export default function Step1() {
                 <MdPublic /> Step 1
             </StepStyles>
             <Form onSubmit={handleSubmit(onSubmit)}>
-                <Input ref={register} id="firstName" type="text" label="First Name" name="firstName" error={!!errors.firstName} helperText={errors?.firstName?.message}/>
-                <Input ref={register} id="lastName" type="text" label="Last Name" name="lastName" error={!!errors.lastName} helperText={errors?.lastName?.message}/>
+                <Input 
+                    ref={register} 
+                    id="firstName" 
+                    type="text" 
+                    label="First Name" 
+                    name="firstName" 
+                    error={!!errors.firstName} 
+                    helperText={errors?.firstName?.message}/>
+                <Input 
+                    ref={register}
+                    id="lastName"
+                    type="text" 
+                    label="Last Name" 
+                    name="lastName" 
+                    error={!!errors.lastName} 
+                    helperText={errors?.lastName?.message}/>
                 <PrimaryButton>Next</PrimaryButton>
             </Form>
         </Layout>
--- src/components/Step2.js
+++ src/components/Step2.js
@@ -1,10 +1,63 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import PrimaryButton from './PrimaryButton'
 import Layout from './Layout';
+import { MdPublic } from 'react-icons/md';
+import styled from 'styled-components';
+import Form from './Form';
+import Input from './Input';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const StepStyles = styled.h2`
+    font-size: 2rem;
+    display: flex;
+    align-items: center;
+`;
+
+// form validation
+// using yup
+
+const schema = yup.object().shape({
+    email: yup
+            .string()
+            .email("Email should have correct format")
+            .required("Email is a required field"),
+
+})
 
 export default function Step2(){
+
+    const history = useHistory();
+
+
+    const { register, handleSubmit, errors, watch } = useForm({
+        mode: "onBlur",
+        resolver: yupResolver(schema)
+    })
+
+    const onSubmit = (data) => {
+        history.push('/step3')
+    }
+
     return (
         <Layout>
-            <h1>Step2</h1>
+            <StepStyles>
+                <MdPublic /> Step 2
+            </StepStyles>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                    ref={register}
+                    id="email"
+                    type="email"
+                    label="Email"
+                    name="email"
+                    required
+                    error={!!errors.email}
+                    helperText={errors?.email?.message} />
+                <PrimaryButton>Next</PrimaryButton>
+            </Form>
         </Layout>
     )
 }
